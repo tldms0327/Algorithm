ec6d3ba5757ebcfd758912a7db5049b5af9ba7a1
Sieun <tldms0327@yonsei.ac.kr>
2021-06-24 00:34:32 +0900

    프로그래머스 네트워크

---
+++ level3_네트워크_dfs.py
@@ -0,0 +1,20 @@
+def solution(n, computer):
+	answer=0
+	while computer !=[[[0]*n]*n]:
+		comp=0
+		for j in range(n):
+			if computer[comp][j]:
+				
+		
+
+
+	return answer
+
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
+n=[[1,2,3],[4,5,6],[7,8,9]]
+print(1 in n)
+print(len(n))
+k=[{1,2,3},{4,5,6}]
+print(k[0], k[1], 1 in k)
+print([[[0]*2]*2])
